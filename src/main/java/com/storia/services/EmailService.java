def463df2ab77f79c245bffa40f550b243fb06db
Hilixx <jhonalizan@gmail.com>
2025-05-02 11:41:44 -0300

    feature: emailService

--- src/main/java/com/storia/services/EmailService.java
+++ src/main/java/com/storia/services/EmailService.java
@@ -1,4 +1,14 @@
 package com.storia.services;
 
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
 public class EmailService {
+
+    @Inject
+    EmailService emailService;
+
+    public List
 }
