104974eb58e4bec0f37c9270403d26220ead5e0c
Hilixx <jhonalizan@gmail.com>
2025-05-02 13:56:01 -0300

    feature: findAll and findById

--- src/main/java/com/storia/services/EmailService.java
+++ src/main/java/com/storia/services/EmailService.java
@@ -1,14 +1,24 @@
 package com.storia.services;
 
 
+import com.storia.models.Email;
+import com.storia.repositories.EmailRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+
+import java.util.List;
 
 @ApplicationScoped
 public class EmailService {
 
     @Inject
-    EmailService emailService;
+    EmailRepository emailRepository;
 
-    public List
+    public List<Email> findAll(){
+        return emailRepository.listAll();
+    }
+    public Email findById(Long id){
+        return  emailRepository.findById(id);
+    }
+
 }
--- src/main/java/com/storia/services/WppService.java
+++ src/main/java/com/storia/services/WppService.java
@@ -1,4 +1,24 @@
 package com.storia.services;
 
+import com.storia.models.WhatsApp;
+import com.storia.repositories.WppRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.List;
+
+@ApplicationScoped
 public class WppService {
+
+    @Inject
+    WppRepository wppRepository;
+
+    public List<WhatsApp> findAll(){
+        return wppRepository.listAll();
+    }
+
+    public WhatsApp findById(long id){
+        return wppRepository.findById(id);
+    }
+
 }
