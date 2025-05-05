b9ae9a3426dfb5655dc072ae7ad3bb9066299920
Hilixx <jhonalizan@gmail.com>
2025-05-05 11:13:17 -0300

    debug: emailService - try catch

--- src/main/java/com/storia/controllers/EmailController.java
+++ src/main/java/com/storia/controllers/EmailController.java
@@ -1,4 +1,35 @@
 package com.storia.controllers;
 
+
+import com.storia.services.EmailService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/movie")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class EmailController {
+
+
+    @Inject
+    EmailService emailService;
+
+    @GET
+    public Response findAll(){
+        return Response.ok(emailService.findAll()).build();
+    }
+    @GET
+    @Path("/{id}")
+    public Response findById(@PathParam("id")long id){
+        return Response.ok(emailService.findById(id)).build();
+    }
+
+    @GET
+    @Path("/read/{filter}")
+    public Response readEmails(@PathParam("filter")String filter){
+        return Response.ok(emailService.readEmail(filter)).build();
+    }
+
 }
--- src/main/java/com/storia/services/EmailService.java
+++ src/main/java/com/storia/services/EmailService.java
@@ -29,17 +29,23 @@ public class EmailService {
     public Email findById(Long id){
         return  emailRepository.findById(id);
     }
-    public List<EmailDto> readEmail(String filter) throws Exception {
-        List<EmailDto> listEmail = emailReader.readEmailWithFilter(filter);
-        for(EmailDto emailDto : listEmail){
-            Email email = new Email();
-            email.setUser(userService.findByEmail(emailDto.userEmail()));
-            email.setTitle(emailDto.title());
-            email.setContent(emailDto.content());
-            email.setSender(emailDto.sender());
-            emailRepository.persist(email);
+    public List<EmailDto> readEmail(String filter){
+        try {
+            List<EmailDto> listEmail = emailReader.readEmailWithFilter(filter);
+            for(EmailDto emailDto : listEmail){
+                Email email = new Email();
+                email.setUser(userService.findByEmail(emailDto.userEmail()));
+                email.setTitle(emailDto.title());
+                email.setContent(emailDto.content());
+                email.setSender(emailDto.sender());
+                emailRepository.persist(email);
+                return listEmail;
+            }
+
+        }catch (Exception e){
+            System.out.println("Error:" + e);
         }
-        return listEmail;
+        return null;
     }
 
 }
